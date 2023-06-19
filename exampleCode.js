b855a0ef875859a48cd2479eea2f99485eff9fad
GustavoGregory <121519118+GustavoGregory@users.noreply.github.com>
2023-06-19 13:26:52 -0300

    Adicionando diversos casos de Else if
    A estrutura fica muito poluída, nestes casos utilizar o condicional Switch Case

--- exampleCode.js
+++ exampleCode.js
@@ -6,21 +6,58 @@ const usuario = {
 
 const mensagemDeBoasVindas1 = "Bem vindo, torcedor Tricolor!";
 const mensagemDeBoasVindas2 = "Bem vindo, torcedor Rubro-Negro!";
-const mensagemDeBoasVindas3 = "Bem vindo, Amante do esporte!";
+const mensagemDeBoasVindas3 = "Bem vindo, torcedor Vascaíno";
+const mensagemDeBoasVindas4 = "Bem vindo, torcedor Santista!";
+const mensagemDeBoasVindas5 = "Bem vindo, torcedor do Náutico!";
+const mensagemDeBoasVindas6 = "Bem vindo, torcedor Colorado!";
+const mensagemDeBoasVindas7 = "Bem vindo, torcedor Gremista!";
+const mensagemDeBoasVindas8 = "Bem vindo, torcedor do Cruzeiro!";
+
+const mensagemDeBoasVindasGeral = "Bem vindo, Amante do esporte!";
 
 //If = condição principal
-if(usuario.time === "Fluminense"){                    
-    console.log(mensagemDeBoasVindas1);
+if (usuario.time === "Fluminense") {
+  console.log(mensagemDeBoasVindas1);
 }
 
 //Else if = condição secundária que ira executar caso condições anteriores não satisfazem
-else if(usuario.time === "Flamengo"){                  
-    console.log(mensagemDeBoasVindas2);
+else if (usuario.time === "Flamengo") {
+  console.log(mensagemDeBoasVindas2);
 }
 
-//Else = executa caso nenhuma condição satisfaz
-else{
+//Else if = condição secundária que ira executar caso condições anteriores não satisfazem
+else if (usuario.time === "Vasco") {
     console.log(mensagemDeBoasVindas3);
+  }
+  
+  //Else if = condição secundária que ira executar caso condições anteriores não satisfazem
+else if (usuario.time === "Santos") {
+    console.log(mensagemDeBoasVindas4);
+  }
+
+  //Else if = condição secundária que ira executar caso condições anteriores não satisfazem
+else if (usuario.time === "Náutico") {
+    console.log(mensagemDeBoasVindas5);
+  }
+
+  //Else if = condição secundária que ira executar caso condições anteriores não satisfazem
+else if (usuario.time === "Internacional") {
+    console.log(mensagemDeBoasVindas6);
+  }
+
+  //Else if = condição secundária que ira executar caso condições anteriores não satisfazem
+else if (usuario.time === "Grêmio") {
+    console.log(mensagemDeBoasVindas7);
+  }
+
+  //Else if = condição secundária que ira executar caso condições anteriores não satisfazem
+else if (usuario.time === "Cruzeiro") {
+    console.log(mensagemDeBoasVindas8);
+  }
+
+//Else = executa caso nenhuma condição satisfaz
+else {
+  console.log(mensagemDeBoasVindasGeral);
 }
 
 const mensagemDeEscolha =
