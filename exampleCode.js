6b6f319745381a37225c49ff586bd5176b36070f
GustavoGregory <121519118+GustavoGregory@users.noreply.github.com>
2023-06-19 11:02:04 -0300

    Exemplo inicial
    Primeiro exemplo sobre condicionais sem nenhuma condicional

---
+++ exampleCode.js
@@ -0,0 +1,11 @@
+const mensagemDeBoasVindas1 = "Bem vindo, torcedor Tricolor!";
+const mensagemDeBoasVindas2 = "Bem vindo, torcedor Tricolor!";
+
+console.log(mensagemDeBoasVindas1);
+console.log(mensagemDeBoasVindas2);
+
+const mensagemDeEscolha = "Clique no setor para o qual deseja comprar o seu ingresso!";
+const mensagemFinal = "Divirta-se no Maracanã !!! ";
+
+console.log(mensagemDeEscolha);
+console.log(mensagemFinal);
