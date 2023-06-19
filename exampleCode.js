199f67d937f52a3dc2b18991f4f0cac341531478
GustavoGregory <121519118+GustavoGregory@users.noreply.github.com>
2023-06-19 13:07:37 -0300

    If; else if; else
    adição das condicionais

--- exampleCode.js
+++ exampleCode.js
@@ -1,10 +1,27 @@
+const usuario = {
+  nome: "Daniel",
+  idade: 29,
+  time: "Fluminense",
+};
+
 const mensagemDeBoasVindas1 = "Bem vindo, torcedor Tricolor!";
-const mensagemDeBoasVindas2 = "Bem vindo, torcedor Tricolor!";
+const mensagemDeBoasVindas2 = "Bem vindo, torcedor Rubro-Negro!";
+const mensagemDeBoasVindas3 = "Bem vindo, Amante do esporte!";
 
-console.log(mensagemDeBoasVindas1);
-console.log(mensagemDeBoasVindas2);
+if(usuario.time === "Fluminense"){
+    console.log(mensagemDeBoasVindas1);
+}
 
-const mensagemDeEscolha = "Clique no setor para o qual deseja comprar o seu ingresso!";
+else if(usuario.time === "Flamengo"){   
+    console.log(mensagemDeBoasVindas2);
+}
+
+else{
+    console.log(mensagemDeBoasVindas3);
+}
+
+const mensagemDeEscolha =
+  "Clique no setor para o qual deseja comprar o seu ingresso!";
 const mensagemFinal = "Divirta-se no Maracanã !!! ";
 
 console.log(mensagemDeEscolha);
