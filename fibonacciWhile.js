257c418d51d6a1692a1f1bf34fc7696cae3c5810
GustavoGregory <121519118+GustavoGregory@users.noreply.github.com>
2023-06-20 17:26:24 -0300

    Sequência de fibonacci
    Sequencia utilizando loop em while

---
+++ fibonacciWhile.js
@@ -0,0 +1,21 @@
+function calculateFibonacciTerm(termNumber) {
+  let lastTerm = 1;
+  let secondToLast = 0;
+
+  if ((termNumber === 0)) {
+    return 0;
+  }
+  if ((termNumber === 1)) {
+    return 1;
+  }
+  let currentIteration = 2;
+  while (currentIteration <= termNumber){
+    let auxVar = lastTerm + secondToLast;
+    secondToLast = lastTerm;
+    lastTerm = auxVar;
+    currentIteration++;
+  }
+  return lastTerm;
+}
+
+console.log(`o valor do termo número 6 da sequencia de fibonacci é ${calculateFibonacciTerm(6)}`)
