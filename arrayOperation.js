cb2cafbaec4d1bad8b995453fe9e0bb24111766d
Gustavo Gregory <gustavogregory235eu@gmail.com>
2023-06-20 17:49:29 -0300

    Array soma o próprio indice

--- arrayOperation.js
+++ arrayOperation.js
@@ -1,3 +1,9 @@
 const regularArray = [2, 5, 2, 7, 1, 3, 4, 7];
 
-let count;
+let count = 0;
+while (count < regularArray.length) {
+  regularArray[count] += count;
+  count++;
+}
+
+console.log("o array atual é:", regularArray);
